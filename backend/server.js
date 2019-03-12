b5f334180d5ddd636b7f18397818c2ead13cbaf9
Nick Hanselman <nickhanselman@icloud.com>
2019-03-12 14:17:50 -0600

    added comments for clarity

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,12 @@ const PORT = 3001;
 const app = express();
 const router = express.Router();
 
+// initialize express body-parsing for error logging
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(logger("dev"));
+
+// database connect link
 const dbConnect =
   "mongodb+srv://chip:skobuffs@chipscorral-v85sn.mongodb.net/test?retryWrites=true";
 
@@ -15,14 +21,10 @@ mongoose.connect(dbConnect, {
   useNewUrlParser: true
 });
 
+// connect to database or throw error
 let db = mongoose.connection;
-
 db.once("open", () => console.log("connected to the database"));
-
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use(logger("dev"));
-
+// start server
 app.listen(PORT, () => console.log(`listening on port ${PORT}`));
