7930f1e08c5ce63a6ceb99aade6a6f47134f1994
Nick Hanselman <nickhanselman@icloud.com>
2019-03-14 15:46:32 -0600

    added do not change warnings

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,8 @@ import * as serviceWorker from "./serviceWorker";
 
 import App from "./App";
 
+// DO NOT CHANGE THIS PAGE
+
 ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
