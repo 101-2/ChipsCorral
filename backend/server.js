46a65ea3fd797c8f1ad66a655c3421e4a3662e1b
Nick Hanselman <nickhanselman@icloud.com>
2019-04-02 13:29:33 -0600

    switched err parser and node-pg to pg-promise

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,14 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const logger = require("morgan");
+const pino = require("express-pino-logger")();
 var colors = require("colors");
 
 require("dotenv").config();
-const { Pool } = require("pg");
+const pgp = require("pg-promise")();
 const bcrypt = require("bcrypt");
 const salt = 10;
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: true
-});
+const db = pgp(process.env.DATABASE_URL);
 
 // initialize express
 const PORT = process.env.PORT || 5000;
@@ -20,72 +17,50 @@ const router = express.Router();
 
 // initialize express body-parsing for error logging
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use(logger("dev"));
-
-pool.on("connect", () => {
-  console.log("connected to database".magenta);
-});
-
-pool.on("error", (err, client) => {
-  console.error("Unexpected error on idle client", err);
-  process.exit(-1);
-});
+app.use(pino);
 
 app.get("/test", (req, res) => {
-  pool.connect().then(client => {
-    return client
-      .query("SELECT * FROM users;")
-      .then(res => {
-        client.release();
-        console.log(res.rows);
-      })
-      .catch(err => {
-        client.release();
-        console.log(err.stack);
-      });
-  });
+  db.any("SELECT * FROM USERS;")
+    .then(data => {
+      console.log(data);
+      res.status(200);
+      res.send("Test passed");
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(400);
+      res.send("Test failed");
+    });
 });
 
 app.put("/user/:user_info", (req, res) => {
   const user_info = JSON.parse(req.params.user_info);
   console.log(`Attempting to create user ${user_info.username}`);
 
-  bcrypt
-    .hash(user_info.password, salt)
-    .then(hash => {
-      var query =
-        "INSERT INTO users(email, password, username) VALUES ('" +
-        user_info.email +
-        "', '" +
-        hash +
-        "', '" +
-        user_info.username +
-        "');";
+  bcrypt.hash(user_info.password, salt).then(hash => {
+    var query =
+      "INSERT INTO users(email, password, username) VALUES ('" +
+      user_info.email +
+      "', '" +
+      hash +
+      "', '" +
+      user_info.username +
+      "');";
 
-      console.log(query.yellow);
-      pool.connect().then(client => {
-        return client
-          .query(query)
-          .then(data => {
-            client.release();
-            console.log(data);
-            res.status(200);
-            res.send(`User ${user_info.username} created`);
-          })
-          .catch(err => {
-            client.release();
-            console.log(err.stack);
-            res.status(400);
-            res.send("Error creating user");
-          });
+    console.log(query.yellow);
+
+    db.any(query)
+      .then(data => {
+        console.log(data);
+        res.status(201);
+        res.send(`User ${user_info.username} created.`);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(400);
+        res.send("Error creating user.");
       });
-    })
-    .catch(err => {
-      res.status(400);
-      res.send("Error Hashing");
-      console.log(err);
-    });
+  });
 });
 
 // start server
