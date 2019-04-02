9c47ecf4f34c6b5c2985956f2a5c6c328adf991d
Nick Hanselman <nickhanselman@icloud.com>
2019-04-02 12:18:23 -0600

    converted backend to postgresql, added create user

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,20 @@
-const mongoose = require("mongoose");
 const express = require("express");
 const bodyParser = require("body-parser");
 const logger = require("morgan");
+var colors = require("colors");
 
-// import models
-const User = require("./models/user");
+require("dotenv").config();
+const { Pool } = require("pg");
+const bcrypt = require("bcrypt");
+const salt = 10;
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
+});
 
 // initialize express
-const PORT = 3001;
+const PORT = process.env.PORT || 5000;
 const app = express();
 const router = express.Router();
 
@@ -16,18 +23,70 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(logger("dev"));
 
-// database connect link
-const dbConnect =
-  "mongodb+srv://chip:skobuffs@chipscorral-v85sn.mongodb.net/test?retryWrites=true";
-
-mongoose.connect(dbConnect, {
-  useNewUrlParser: true
+pool.on("connect", () => {
+  console.log("connected to database".magenta);
 });
 
-// connect to database or throw error
-let db = mongoose.connection;
-db.once("open", () => console.log("connected to the database"));
-db.on("error", console.error.bind(console, "MongoDB connection error:"));
+pool.on("error", (err, client) => {
+  console.error("Unexpected error on idle client", err);
+  process.exit(-1);
+});
+
+app.get("/test", (req, res) => {
+  pool.connect().then(client => {
+    return client
+      .query("SELECT * FROM users;")
+      .then(res => {
+        client.release();
+        console.log(res.rows);
+      })
+      .catch(err => {
+        client.release();
+        console.log(err.stack);
+      });
+  });
+});
+
+app.put("/user/:user_info", (req, res) => {
+  const user_info = JSON.parse(req.params.user_info);
+  console.log(`Attempting to create user ${user_info.username}`);
+
+  bcrypt
+    .hash(user_info.password, salt)
+    .then(hash => {
+      var query =
+        "INSERT INTO users(email, password, username) VALUES ('" +
+        user_info.email +
+        "', '" +
+        hash +
+        "', '" +
+        user_info.username +
+        "');";
+
+      console.log(query.yellow);
+      pool.connect().then(client => {
+        return client
+          .query(query)
+          .then(data => {
+            client.release();
+            console.log(data);
+            res.status(200);
+            res.send(`User ${user_info.username} created`);
+          })
+          .catch(err => {
+            client.release();
+            console.log(err.stack);
+            res.status(400);
+            res.send("Error creating user");
+          });
+      });
+    })
+    .catch(err => {
+      res.status(400);
+      res.send("Error Hashing");
+      console.log(err);
+    });
+});
 
 // start server
-app.listen(PORT, () => console.log(`listening on port ${PORT}`));
+app.listen(PORT, () => console.log(`listening on port ${PORT}`.magenta));
