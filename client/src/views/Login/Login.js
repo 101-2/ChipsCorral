170ed25482c5f95126d159b27b11c323f6f7327e
Nick Hanselman <nickhanselman@icloud.com>
2019-04-02 12:19:47 -0600

    added to login page to test

--- client/src/views/Login/Login.js
+++ client/src/views/Login/Login.js
@@ -1,8 +1,30 @@
 import React, { Component } from "react";
+const axios = require("axios");
+
+var baseURL = "http://localhost:3000";
 
 class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+    this.test = this.test.bind(this);
+  }
+
+  test(event) {
+    axios
+      .get(baseURL + "/test")
+      .then(data => console.log(data))
+      .catch(err => console.log(err));
+  }
+
   render() {
-    return <div>Login</div>;
+    return (
+      <div>
+        <button className="btn btn-success" type="button" onClick={this.test}>
+          Test
+        </button>
+      </div>
+    );
   }
 }
 
