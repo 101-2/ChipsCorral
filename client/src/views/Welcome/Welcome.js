e83e468c6dafeb5c7c1406b6f0338df2c7aa8a8a
Nick Hanselman <nickhanselman@icloud.com>
2019-04-16 12:18:31 -0600

    formatting changes

--- client/src/views/Welcome/Welcome.js
+++ client/src/views/Welcome/Welcome.js
@@ -13,14 +13,15 @@ class Welcome extends Component {
   render() {
     return (
       <div>
-        <img className="bg" src="http://www.desktopimages.org/pictures/2015/0502/1/flatirons-wallpaper-84736.jpg"></img>
-        <img className="centered" src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Colorado_Buffaloes_wordmark.svg/1200px-Colorado_Buffaloes_wordmark.svg.png"></img>
-    	
-      
-      
-      
+        <img
+          className="bg"
+          src="http://www.desktopimages.org/pictures/2015/0502/1/flatirons-wallpaper-84736.jpg"
+        />
+        <img
+          className="centered"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Colorado_Buffaloes_wordmark.svg/1200px-Colorado_Buffaloes_wordmark.svg.png"
+        />
       </div>
-
     );
   }
 }
