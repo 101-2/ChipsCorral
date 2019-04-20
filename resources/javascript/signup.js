ba78db8486626379c9483edbd3c7d14097bfc4c8
Nick Hanselman <nickhanselman@icloud.com>
2019-04-20 15:59:47 -0600

    switched content type

--- resources/javascript/signup.js
+++ resources/javascript/signup.js
@@ -31,7 +31,7 @@ function createUser() {
       method: "POST",
       body: params,
       headers: {
-        "Content-Type": "application/x-www-form-urlencoded"
+        "Content-Type": "application/json"
       }
     })
       .then(res => res.json())
