7c7dfa5a846e17a24d23080c3c1463b8788c0b7b
Nick Hanselman <nickhanselman@icloud.com>
2019-04-20 14:24:22 -0600

    created signup functions

---
+++ resources/javascript/signup.js
@@ -0,0 +1,38 @@
+const axios = require("axios");
+
+const baseUrl = "https://cub-forum.herokuapp.com";
+
+function checkEmail(email) {
+  var email_regex = RegExp("^[A-Za-z0-9._%+-]+@colorado.edu$");
+  if (email_regex.test(email)) {
+    console.log(`${email} is valid`);
+    return true;
+  } else {
+    console.log("Must be a @colorado.edu email");
+    return false;
+  }
+}
+
+function createUser() {
+  var new_email = document.getElementById("inputEmail").value;
+  var new_password = document.getElementById("inputPassword").value;
+  var new_username = document.getElementById("inputUsername").value;
+
+  console.log(new_email + " " + new_password + " " + new_username);
+
+  if (checkEmail(new_email)) {
+    var params = {
+      email: new_email,
+      password: new_password,
+      username: new_username
+    };
+
+    axios
+      .post(`${baseUrl}/user`, params)
+      .then(data => console.log("DATA: " + data))
+      .then((window.location = `${baseUrl}/home`))
+      .catch(err => console.log("ERR: " + err));
+  } else {
+    window.alert("Must enter a @colorado.edu email");
+  }
+}
