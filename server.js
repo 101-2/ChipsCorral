deaa20fd85f056e205888fad2c1763a3bf816538
Nick Hanselman <nickhanselman@icloud.com>
2019-04-20 17:18:06 -0600

    trying to remove network error

--- server.js
+++ server.js
@@ -19,6 +19,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
