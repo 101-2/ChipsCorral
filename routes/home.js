c1ca0134046c6f07c06b4094b038848750d8c0b8
Nick Hanselman <nickhanselman@icloud.com>
2019-04-21 17:14:15 -0600

    fixed typo

--- routes/home.js
+++ routes/home.js
@@ -3,7 +3,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res, render("pages/home");
+  res.render("pages/home");
 });
 
 module.exports = router;
