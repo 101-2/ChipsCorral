6e658c254f178674f2cd5bf8d65127688af27747
Nick Hanselman <nickhanselman@icloud.com>
2019-04-22 17:15:11 -0600

    removed unneeded console.log

--- routes/home.js
+++ routes/home.js
@@ -3,7 +3,6 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  console.log(req.user);
   res.render("pages/home", { name: req.user.displayName });
 });
 
