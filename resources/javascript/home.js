07ed6b04afe6a63466d117e1560b13be6667bc78
Nick Hanselman <nickhanselman@icloud.com>
2019-04-22 13:31:27 -0600

    function for populating post-container

--- resources/javascript/home.js
+++ resources/javascript/home.js
@@ -1,8 +1,35 @@
 function getPosts() {
+  var posts = "";
   axios
     .get("https://cub-forum.herokuapp.com/posts")
     .then(data => {
       console.log(data);
+      for (var i = 0; i < data.length; i++) {
+        posts += `
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">${data[i].title}</h5>
+            <h6 class="card-subtitle mb-2 text-muted">${
+              getUser(data[i].user_id).displayName
+            }</h6>
+            <p class="card-text">${data[i].content}</p>
+          </div>
+        </div>
+        `;
+      }
+      document.getElementById("post-container").innerHTML = posts;
+    })
+    .catch(err => {
+      console.error(err);
+    });
+}
+
+function getUser(user_id) {
+  axios
+    .get(`https://cub-forum.herokuapp.com/user?user_id=${user_id}`)
+    .then(data => {
+      console.log(data);
+      return data;
     })
     .catch(err => {
       console.error(err);
