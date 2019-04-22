33e859544642b9c8343d7ea8eaf327cb72a15a93
Nick Hanselman <nickhanselman@icloud.com>
2019-04-22 14:10:42 -0600

    implementing getUser inside getPost

--- resources/javascript/home.js
+++ resources/javascript/home.js
@@ -17,18 +17,25 @@ function getPosts() {
     .get("https://cub-forum.herokuapp.com/posts")
     .then(obj => {
       console.log(obj.data);
-      var user = getUser(obj.data[0].user_id);
-      console.log("USER: " + user);
       for (var i = 0; i < obj.data.length; i++) {
-        posts += `
-        <div class="card card-format">
-          <div class="card-body">
-            <h5 class="card-title">${obj.data[i].title}</h5>
-            <h6 class="card-subtitle mb-2 text-muted">name</h6>
-            <p class="card-text">${obj.data[i].content}</p>
+        axios
+          .get(`https://cub-forum.herokuapp.com/user?user_id=${user_id}`)
+          .then(user => {
+            posts += `
+          <div class="card card-format">
+            <div class="card-body">
+              <h5 class="card-title">${obj.data[i].title}</h5>
+              <h6 class="card-subtitle mb-2 text-muted">${
+                user.data.displayName
+              }</h6>
+              <p class="card-text">${obj.data[i].content}</p>
+            </div>
           </div>
-        </div>
-        `;
+          `;
+          })
+          .catch(err => {
+            console.log(err);
+          });
       }
       console.log(posts);
       document.getElementById("post-container").innerHTML = posts;
