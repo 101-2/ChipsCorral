24146bc70180a8736965d6ea82e61a4afe2d8ef6
Nick Hanselman <nickhanselman@icloud.com>
2019-04-22 13:37:31 -0600

    reformatted obj

--- resources/javascript/home.js
+++ resources/javascript/home.js
@@ -2,21 +2,22 @@ function getPosts() {
   var posts = "";
   axios
     .get("https://cub-forum.herokuapp.com/posts")
-    .then(data => {
-      console.log(data);
-      for (var i = 0; i < data.length; i++) {
+    .then(obj => {
+      console.log(obj.data);
+      for (var i = 0; i < obj.data.length; i++) {
         posts += `
         <div class="card">
           <div class="card-body">
-            <h5 class="card-title">${data[i].title}</h5>
+            <h5 class="card-title">${obj.data[i].title}</h5>
             <h6 class="card-subtitle mb-2 text-muted">${
-              getUser(data[i].user_id).displayName
+              getUser(obj.data[i].user_id).displayName
             }</h6>
-            <p class="card-text">${data[i].content}</p>
+            <p class="card-text">${obj.data[i].content}</p>
           </div>
         </div>
         `;
       }
+      console.log(posts);
       document.getElementById("post-container").innerHTML = posts;
     })
     .catch(err => {
